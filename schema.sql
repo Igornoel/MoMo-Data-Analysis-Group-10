6bce63b8f1bf1e005c630243eec5532c16a91163
Igornoel <n.ishimwe4@alustudent.com>
2025-06-15 12:20:37 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,15 @@
 CREATE TABLE transactions (
-    CATEGORY TEXT,
-    AMOUNT INTEGER,
-    RECIPIENT TEXT,
-    TRANSACTION_TYPE TEXT,
-    date TIMESTAMP,
-    MESSAGE_BODY TEXT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    category TEXT NOT NULL,
+    amount INTEGER NOT NULL,
+    sender TEXT,
+    recipient TEXT,
+    transaction_id TEXT,
+    agent_name TEXT,
+    agent_phone TEXT,
+    bundle_size INTEGER,
+    validity_days INTEGER,
+    date DATETIME NOT NULL
 );
 
 CREATE TABLE logs (
